e081ca66746e94053405ad74274e65f281d8fdb5
HorskyUniverse <33571651+HorskyUnivers@users.noreply.github.com>
2022-05-06 11:39:40 +0800

    20220506data
    add some data for test

---
+++ test/220418test.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+echo "wordassociation-2011.txt 3_p.txt========================================" >> 220418test.txt
+./main /home/maty/DataSet/wordassociation-2011.txt 3_p.txt 10617 3 output.txt >> 220418test.txt
+echo "wordassociation-2011.txt 4_p.txt========================================" >> 220418test.txt
+./main /home/maty/DataSet/wordassociation-2011.txt 4_p.txt 10617 4 output.txt >> 220418test.txt
+echo "wordassociation-2011.txt 5_p.txt========================================" >> 220418test.txt
+./main /home/maty/DataSet/wordassociation-2011.txt 5_p.txt 10617 5 output.txt >> 220418test.txt
+echo "wordassociation-2011.txt 6_p.txt========================================" >> 220418test.txt
+./main /home/maty/DataSet/wordassociation-2011.txt 6_p.txt 10617 6 output.txt >> 220418test.txt
+echo "wordassociation-2011.txt 7_p.txt========================================" >> 220418test.txt
+./main /home/maty/DataSet/wordassociation-2011.txt 7_p.txt 10617 7 output.txt >> 220418test.txt
+echo "enron.txt 3_p.txt========================================" >> 220418test.txt
+./main /home/maty/DataSet/enron.txt 3_p.txt 69244 3 output.txt >> 220418test.txt
+echo "enron.txt 4_p.txt========================================" >> 220418test.txt
+./main /home/maty/DataSet/enron.txt 4_p.txt 69244 4 output.txt >> 220418test.txt
+echo "enron.txt 5_p.txt========================================" >> 220418test.txt
+./main /home/maty/DataSet/enron.txt 5_p.txt 69244 5 output.txt >> 220418test.txt
+echo "enron.txt 6_p.txt========================================" >> 220418test.txt
+./main /home/maty/DataSet/enron.txt 6_p.txt 69244 6 output.txt >> 220418test.txt
+echo "enron.txt 7_p.txt========================================" >> 220418test.txt
+./main /home/maty/DataSet/enron.txt 7_p.txt 69244 7 output.txt >> 220418test.txt
+echo "dblp-2010.txt 3_p.txt========================================" >> 220418test.txt
+./main /home/maty/DataSet/dblp-2010.txt 3_p.txt 326186 3 output.txt >> 220418test.txt
+echo "dblp-2010.txt 4_p.txt========================================" >> 220418test.txt
+./main /home/maty/DataSet/dblp-2010.txt 4_p.txt 326186 4 output.txt >> 220418test.txt
+echo "dblp-2010.txt 5_p.txt========================================" >> 220418test.txt
+./main /home/maty/DataSet/dblp-2010.txt 5_p.txt 326186 5 output.txt >> 220418test.txt
+echo "dblp-2010.txt 6_p.txt========================================" >> 220418test.txt
+./main /home/maty/DataSet/dblp-2010.txt 6_p.txt 326186 6 output.txt >> 220418test.txt
+echo "dblp-2010.txt 7_p.txt========================================" >> 220418test.txt
+./main /home/maty/DataSet/dblp-2010.txt 7_p.txt 326186 7 output.txt >> 220418test.txt
+echo "itwiki-2013.txt 3_p.txt========================================" >> 220418test.txt
+./main /home/maty/DataSet/itwiki-2013.txt 3_p.txt 1016867 3 output.txt >> 220418test.txt
+echo "itwiki-2013.txt 4_p.txt========================================" >> 220418test.txt
+./main /home/maty/DataSet/itwiki-2013.txt 4_p.txt 1016867 4 output.txt >> 220418test.txt
+echo "itwiki-2013.txt 5_p.txt========================================" >> 220418test.txt
+./main /home/maty/DataSet/itwiki-2013.txt 5_p.txt 1016867 5 output.txt >> 220418test.txt
+echo "itwiki-2013.txt 6_p.txt========================================" >> 220418test.txt
+./main /home/maty/DataSet/itwiki-2013.txt 6_p.txt 1016867 6 output.txt >> 220418test.txt
+echo "itwiki-2013.txt 7_p.txt========================================" >> 220418test.txt
+./main /home/maty/DataSet/itwiki-2013.txt 7_p.txt 1016867 7 output.txt >> 220418test.txt
+echo "enwiki-2020.txt 3_p.txt========================================" >> 220418test.txt
+./main /home/maty/DataSet/enwiki-2020.txt 3_p.txt 6047510 3 output.txt >> 220418test.txt
+echo "enwiki-2020.txt 4_p.txt========================================" >> 220418test.txt
+./main /home/maty/DataSet/enwiki-2020.txt 4_p.txt 6047510 4 output.txt >> 220418test.txt
+echo "enwiki-2020.txt 5_p.txt========================================" >> 220418test.txt
+./main /home/maty/DataSet/enwiki-2020.txt 5_p.txt 6047510 5 output.txt >> 220418test.txt
+echo "enwiki-2020.txt 6_p.txt========================================" >> 220418test.txt
+./main /home/maty/DataSet/enwiki-2020.txt 6_p.txt 6047510 6 output.txt >> 220418test.txt
+echo "enwiki-2020.txt 7_p.txt========================================" >> 220418test.txt
+./main /home/maty/DataSet/enwiki-2020.txt 7_p.txt 6047510 7 output.txt >> 220418test.txt
+
---
+++ test/220419test.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+echo "wordassociation-2011.txt 4_p2.txt========================================" >> 220419test.txt
+./main /home/maty/DataSet/wordassociation-2011.txt 4_p2.txt 10617 4 output.txt >> 220419test.txt
+echo "wordassociation-2011.txt 6_p.txt========================================" >> 220419test.txt
+./main /home/maty/DataSet/wordassociation-2011.txt 6_p.txt 10617 6 output.txt >> 220419test.txt
+echo "wordassociation-2011.txt 7_p.txt========================================" >> 220419test.txt
+./main /home/maty/DataSet/wordassociation-2011.txt 7_p.txt 10617 7 output.txt >> 220419test.txt
+echo "enron.txt 4_p2.txt========================================" >> 220419test.txt
+./main /home/maty/DataSet/enron.txt 4_p2.txt 69244 4 output.txt >> 220419test.txt
+echo "enron.txt 6_p.txt========================================" >> 220419test.txt
+./main /home/maty/DataSet/enron.txt 6_p.txt 69244 6 output.txt >> 220419test.txt
+echo "enron.txt 7_p.txt========================================" >> 220419test.txt
+./main /home/maty/DataSet/enron.txt 7_p.txt 69244 7 output.txt >> 220419test.txt
+echo "dblp-2010.txt 4_p2.txt========================================" >> 220419test.txt
+./main /home/maty/DataSet/dblp-2010.txt 4_p2.txt 326186 4 output.txt >> 220419test.txt
+echo "dblp-2010.txt 6_p.txt========================================" >> 220419test.txt
+./main /home/maty/DataSet/dblp-2010.txt 6_p.txt 326186 6 output.txt >> 220419test.txt
+echo "dblp-2010.txt 7_p.txt========================================" >> 220419test.txt
+./main /home/maty/DataSet/dblp-2010.txt 7_p.txt 326186 7 output.txt >> 220419test.txt
+echo "itwiki-2013.txt 4_p2.txt========================================" >> 220419test.txt
+./main /home/maty/DataSet/itwiki-2013.txt 4_p2.txt 1016867 4 output.txt >> 220419test.txt
+echo "itwiki-2013.txt 6_p.txt========================================" >> 220419test.txt
+./main /home/maty/DataSet/itwiki-2013.txt 6_p.txt 1016867 6 output.txt >> 220419test.txt
+echo "itwiki-2013.txt 7_p.txt========================================" >> 220419test.txt
